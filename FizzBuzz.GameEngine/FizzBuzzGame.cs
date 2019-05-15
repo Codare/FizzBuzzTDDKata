1511463f95ba8a9c76b1b166442b5b904b620017
Adrian Cooke <adrian@rightindem.com>
2019-05-15 15:10:07 +0100

    Alternate return sooner method of doing Fizz Buzz.

--- FizzBuzz.GameEngine/FizzBuzzGame.cs
+++ FizzBuzz.GameEngine/FizzBuzzGame.cs
@@ -9,26 +9,48 @@
                 return "0";
             }
 
-            string s = "";
-
-            if (iteration % 3 == 0)
+            if (iteration % 3 == 0 && iteration % 5 != 0)
             {
-                s = "Fizz";
+                return "Fizz";
             }
 
-            if (iteration % 5 == 0)
+            if (iteration % 5 == 0 && iteration % 3 != 0)
             {
-                s = "Buzz";
+                return "Buzz";
             }
 
             if (iteration % 3 == 0 && iteration % 5 == 0)
             {
-                s = "FizzBuzz";
+                return "FizzBuzz";
             }
 
-            return string.IsNullOrWhiteSpace(s)
-                ? iteration.ToString()
-                : s;
+            return iteration.ToString();
+
+            //if (iteration == 0)
+            //{
+            //    return "0";
+            //}
+
+            //string s = "";
+
+            //if (iteration % 3 == 0)
+            //{
+            //    s = "Fizz";
+            //}
+
+            //if (iteration % 5 == 0)
+            //{
+            //    s = "Buzz";
+            //}
+
+            //if (iteration % 3 == 0 && iteration % 5 == 0)
+            //{
+            //    s = "FizzBuzz";
+            //}
+
+            //return string.IsNullOrWhiteSpace(s)
+            //    ? iteration.ToString()
+            //    : s;
         }
     }
 }
